a66a357922543a927b702ebf252eb89dbb7f2161
mitika903 <mitika.modi24@gmail.com>
2026-06-24 22:56:14 +0530

    Updated Mitika profile details

--- REACTCODE/ClassStart/src/App.jsx
+++ REACTCODE/ClassStart/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-8xl text-blue-500">
-      hello my name is Deewakar and i am learing react and tailwindcss with vite
+    <div className="p-10 space-y-4 text-blue-500">
+      <h1 className="text-6xl font-bold">Hello, my name is Mitika</h1>
+      <p className="text-4xl">Degree: Bachelor of Computer Applications (BCA)</p>
+      <p className="text-4xl">Roll No: 24032211191</p>
     </div>
   )
 }
