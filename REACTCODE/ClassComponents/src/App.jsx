4688a11f5b5c8aec702e6f14343a71cd9d48d290
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-06-25 03:00:57 +0530

    Named Export vs Default Export and Staic classes Vs Dynamic class

--- REACTCODE/ClassComponents/src/App.jsx
+++ REACTCODE/ClassComponents/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import Button from './Components/Button'
+import { Button, Button1, Button2 } from './Components/Button'
+// import { Button } from './Components/Button'
 import Studentscard from './Components/Studentscard'
-import InstagramCard from './Components/InstagramCard'
-import NetflixSeries from './Components/NetflixCard'
+// import InstagramCard from './Components/InstagramCard'
+// import NetflixSeries from './Components/NetflixCard'
 
 const App = () => {
   return (
     <>
-      {/* <Button text="Buy Now" /> */}
-      {/* <Button text="Add to Cart" /> */}
+      <Button text="Add to Cart" />
+      <Button2 text="Buy Now" />
+      <Button1 text="Subscribe" />
+
+      {/* <Button text=" Add to Cart" className="bg-green-500" />
+      <Button text="Buy Now" className="bg-amber-500" />
+      <Button text="Subscribe" className="bg-red-500" /> */}
 
 
       {/* <Button /> */}
--- REACTCODE/ClassComponents/src/Components/Button.jsx
+++ REACTCODE/ClassComponents/src/Components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ text }) => {
+export const Button = ({ text }) => {
     return (
         <div className="flex items-center justify-center gap-4 mt-5">
             <button className="px-6 py-3 bg-green-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
@@ -10,5 +10,42 @@ const Button = ({ text }) => {
         </div>
     )
 }
+export const Button2 = ({ text }) => {
+    return (
+        <div className="flex items-center justify-center gap-4 mt-5">
+            <button className="px-6 py-3 bg-red-500 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+                {text}
+            </button>
 
-export default Button
+        </div>
+    )
+}
+
+
+export const Button1 = ({ text }) => {
+    return (
+        <div className="flex items-center justify-center gap-4 mt-5">
+            <button
+                className="px-6 py-3 text-white font-medium bg-amber-300 rounded-lg shadow-md hover:bg-blue-700 transition duration-300 "
+            >
+                {text}
+            </button>
+        </div >
+    )
+}
+
+// export const Button = ({ text, className }) => {
+//     return (
+//         <div className="flex items-center justify-center gap-4 mt-5">
+//             <button
+//                 className={`px-6 py-3 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 transition duration-300 ${className}`}
+//             >
+//                 {text}
+//             </button>
+//         </div>
+//     )
+// }
+
+
+
+
