0fdeec608ccbf4ede2f5bebf16945cfae6f95b35
harssh-workspace <harshp18072004@gmail.com>
2026-07-01 14:46:47 +0530

    comment second student details

--- REACTCODE/StudentCard/src/App.jsx
+++ REACTCODE/StudentCard/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const studentDetails = [
     {category: 'STUDENT', logo: guniLogo, studentPhoto: student0126, studentName: 'PRAJAPATI HARSH DINESHKUMAR', EnrollNo: '24032211126', TEnrollNo: "T24032211095", college: 'AMPICS BCA(Hons.)', bGroup: '', mobile: '6356465555', validity: '27/05/2028', barcode: barcode0126},
-    {category: 'STUDENT', logo: guniLogo, studentPhoto: student085, studentName: 'PATEL BRIJESH ARAVINDBHAI', EnrollNo: '24032211085', TEnrollNo: "T24032211096", college: 'AMPICS BCA(Hons.)', bGroup: 'O +', mobile: '7867546862', validity: '23/05/2028', barcode: barcode0126}
+    // {category: 'STUDENT', logo: guniLogo, studentPhoto: student085, studentName: 'PATEL BRIJESH ARAVINDBHAI', EnrollNo: '24032211085', TEnrollNo: "T24032211096", college: 'AMPICS BCA(Hons.)', bGroup: 'O +', mobile: '7867546862', validity: '23/05/2028', barcode: barcode0126}
   ]
 
   return (
